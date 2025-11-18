e4c3d497d873973cc0c5bc1e4b94dd3b8924380c
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-11-18 12:30:55 +0100

    feat: headset(product's subclass) w/ getters & setters

--- src/org/lessons/java/Headset.java
+++ src/org/lessons/java/Headset.java
@@ -1,5 +1,24 @@
 package org.lessons.java;
 
 public class Headset {
+  private String color;
+  private boolean isWireless;
 
+  // getter
+  public String getColor(){
+    return this.color;
+  } 
+
+  public boolean getIsWireless(){
+    return this.isWireless;
+  }
+
+  // setter
+  public void setColor(String newColor){
+    this.color = newColor;
+  }
+
+  public void setIsWireless(boolean newIsWireless){
+    this.isWireless = newIsWireless;
+  }
 }
