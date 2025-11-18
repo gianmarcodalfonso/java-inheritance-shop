a0083d4399990fc5570b8f48fa11ea6b09241cca
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-11-18 12:25:59 +0100

    feat: television(product's subclass) w/ getters & setters

--- src/org/lessons/java/Television.java
+++ src/org/lessons/java/Television.java
@@ -1,5 +1,43 @@
 package org.lessons.java;
 
-public class Television {
+public class Television extends Product{
+  private int base;
+  private int height;
+  private int depth;
+  private boolean isSmart;
+
+  // getter
+  public int getBase(){
+    return this.base;
+  }
+  
+  public int getHeight(){
+    return this.height;
+  }
+  
+  public int getdepth(){
+    return this.depth;
+  }
+
+  public boolean getIsSmart(){
+    return this.isSmart;
+  }
+
+  // setter
+  public void setBase(int newBase){
+    this.base = newBase;
+  }
+  
+  public void setHeight(int newHeight){
+    this.height = newHeight;
+  }
+  
+  public void setDepth(int newDepth){
+    this.depth = newDepth;
+  }
+
+  public void setIsSmart(boolean newIsSmart){
+    this.isSmart = newIsSmart;
+  }
 
 }
