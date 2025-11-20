f79c4c4355037896834b9e02abe618790855f3d1
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-11-20 14:25:20 +0100

    feat: enhance product input handling for Smartphone and Television in Cart

--- src/org/lessons/java/Cart.java
+++ src/org/lessons/java/Cart.java
@@ -9,10 +9,11 @@ public class Cart {
   System.out.println("Seleziona smartphone(1), Televisione(2) o Cuffie(3), attraverso il numero correlato");
   int selectedProduct = in.nextInt();
   if(selectedProduct == 1){
-    System.out.println("Inserisci i dati del prodotto che desideri acquistare");
+    System.out.println("Inserisci i dati dello smartphone che desideri acquistare");
     //chiedo numero di serie
     System.out.println("Numero di serie:");
     int userSerialCode = in.nextInt();
+    in.nextLine();
     //chiedo nome del prodotto
     System.out.println("Nome:");
     String userProductName = in.nextLine();
@@ -34,7 +35,43 @@ public class Cart {
 
 
     Smartphone cartSmartphone = new Smartphone(userSerialCode, userProductName, userProductBrand, userProductPrice, userProductIva, userProductImei, userProductMemory);
-    
+    System.out.println(cartSmartphone.toString());
+  }
+
+  if(selectedProduct == 2){
+    System.out.println("Inserisci i dati della televisione che desideri acquistare");
+    //chiedo numero di serie
+    System.out.println("Numero di serie:");
+    int userSerialCode = in.nextInt();
+    in.nextLine();
+    //chiedo nome del prodotto
+    System.out.println("Nome:");
+    String userProductName = in.nextLine();
+    //chiedo brand del prodotto
+    System.out.println("Brand:");
+    String userProductBrand = in.nextLine();
+    //chiedo prezzo
+    System.out.println("Prezzo:");
+    BigDecimal userProductPrice = in.nextBigDecimal();
+    //chiedo iva
+    System.out.println("Iva:");
+    float userProductIva = in.nextFloat();
+    //chiedo base
+    System.out.println("Misura base:");
+    int userProductBase = in.nextInt();
+    //chiedo altezza
+    System.out.println("Misura altezza:");
+    int userProductHeight = in.nextInt();
+    //chiedo profondità
+    System.out.println("Misura profondità:");
+    int userProductDepth = in.nextInt();
+    //chiedo se è smart o no
+    System.out.println("È smart? Sì (1), No (2):");
+    boolean userProductIsSmart = in.nextInt() == 1;
+
+
+    Television cartTelevision = new Television(userSerialCode, userProductName, userProductBrand, userProductPrice, userProductIva, userProductBase, userProductHeight, userProductDepth, userProductIsSmart);
+    System.out.println(cartTelevision.toString());
   }
 }
 }
